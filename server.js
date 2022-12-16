bbe06590f42e53f0697d0e774b7193f9a5a4316b
aztecbinaynay <qjeabinay@tip.edu.ph>
2022-12-17 03:28:00 +0800

    nothing to add just pushing because building of the server remotely failed

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.put("/course/lessonplanner/update", (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port. ${port}`);
 });
 
 // console.log(PORT);
